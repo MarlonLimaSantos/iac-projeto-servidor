337c5a0b2d5b9ee36cbc042584fc4b00311c3f2a
Marlon <marlon.lsantox@gmail.com>
2023-01-16 22:30:05 -0300

    iac apache vs0.0.1

---
+++ script_iac.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+apt-get update -y
+apt-get upgrade -y 
+apt install apache2 -y
+apt install  unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip 
+unzip main.zip
+cd linux-site-dio-main A
+cp -R * /var/www/html/
+
+echo "Finalizando script..."
